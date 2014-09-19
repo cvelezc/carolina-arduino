7801431c013509277b98ac92bcacdf748ca41184
cvelezc <cvelezc@unal.edu.co>
2014-09-19 15:43:21 -0500

    Metodo parpadeo

--- ClaseArduino/ClaseArduino.ino
+++ ClaseArduino/ClaseArduino.ino
@@ -7,7 +7,7 @@ void loop() {
   digitalWrite(13,LOW);
   delay(1000);
 }
-
+//Metodo parpadeo
 void parpadeo (int time) {
   digitalWrite(13,HIGH);
   delay(1000);
@@ -15,11 +15,27 @@ void parpadeo (int time) {
   delay(time);
 }
   
-
+*/
 void suma(int value) {
   int suma=value+100;
   return suma;
 }
   
-
+  String instring ="" ; 
+  void setup() {
+  Serial.begin(9600);
+  Serial.println("\n\nString toInt():");
+  Serial.println();
+  }
+  void loop() {
+  while (Serial.available() > 0) {
+  int inChar = Serial.read();
+  if (isDigit(inChar)) {
+  inString += (char)inChar;
+  }
+  if (inChar == '\n') {
+  Serial.print("Value:");
+  Serial
+  
+**/
 
